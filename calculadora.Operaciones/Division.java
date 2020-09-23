eb73a5e932cfacca512d1c36722456c93aa4ee03
AbellBlack <71736208+AbellBlack@users.noreply.github.com>
2020-09-23 17:22:32 -0500

    Create Division.java

---
+++ calculadora.Operaciones/Division.java
@@ -0,0 +1,17 @@
+package calculadora.Operaciones;
+
+public class Divicion extends Operacion{
+    public Divicion(double numero1, double numero2) {
+        super(numero1, numero2);
+    }
+    
+    public double getResultado() {
+        return super.getNumero1() / super.getNumero2();
+    }
+    
+    @Override
+    public String toString() {
+        return super.getNumero1() + " / " + super.getNumero2()
+               + " = " + this.getResultado();
+    }
+}
