89ac0b2b94c7922bbd232c71fb4ed70c5679520d
AbellBlack <71736208+AbellBlack@users.noreply.github.com>
2020-09-23 17:20:06 -0500

    Create Operacion.java

---
+++ calculadora.Operaciones/Operacion.java
@@ -0,0 +1,41 @@
+package calculadora.Operaciones;
+
+public abstract class Operacion {
+    private double numero1;
+    private double numero2;
+
+    public Operacion() {
+        this.numero1 = this.numero2 = 1;
+    }
+
+    public Operacion(double numero1, double numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public double getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(double numero1) {
+        this.numero1 = numero1;
+    }
+
+    public double getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(double numero2) {
+        this.numero2 = numero2;
+    }
+
+    
+    
+    @Override
+    public String toString() {
+        return "Operaciones Básicas{" + "operando1 = " + numero1
+                + ", operando2 = " + numero2 + '}';
+    }
+
+   
+}
